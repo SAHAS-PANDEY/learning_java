e603e3de4d87ab2447fb04ea58c88097817c670c
SAHAS-PANDEY <sahaspandey4301@gmail.com>
2022-06-01 12:45:00 +0530

    testt commit

---
+++ src/ap.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class ap {
+    public static int nthterm(int a,int d,int n)
+    {
+        int ans=a+(n-1)*d;
+        return ans;
+    }
+    public static void main(String[]args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the values of a,d,n");
+        int a=sc.nextInt();
+        int d=sc.nextInt();
+        int n=sc.nextInt();
+        System.out.println(nthterm(a,d,n));
+
+    }
+}
