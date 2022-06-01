a7a10f03d43ae6fbc7a94dd403d7f2b1b62146a8
SAHAS-PANDEY <sahaspandey4301@gmail.com>
2022-06-01 13:18:02 +0530

    m test kar rha hun

---
+++ src/lcm.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class lcm
+{
+    public static void main(String[]args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter two numbers");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int x = Math.max(a, b);
+        int y=a*b;
+            int ans = x;
+            for (int i = x; i <= y; i++)
+            {
+                if (i % a == 0 && i % b==0) {
+                    ans = i;
+                    break;
+                }
+            }
+            System.out.println("LCM IS\t" + ans);
+        }
+
+}
+
